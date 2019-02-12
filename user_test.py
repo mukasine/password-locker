0f2dcc568c9d7f5b09b8eac9911ce3a2643bb1b1
mukasine marie claire <mclaremukasine@gmail.com>
2019-02-12 10:24:28 +0200

    add second test

--- user_test.py
+++ user_test.py
@@ -9,3 +9,33 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+    # Items up here .......
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("marie claire","mukasine","0782586013","mclaremukasine@gmail.com") # create user object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"marie claire")
+        self.assertEqual(self.new_user.last_name,"mukasine")
+        self.assertEqual(self.new_user.phone_number,"0782586013")
+        self.assertEqual(self.new_user.email,"mclaremukasine@gmail.com")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+
+
+if __name__ == '__main__':
+    unittest.main()
